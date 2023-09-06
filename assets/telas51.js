037f46c83b1d230f90dff97fba8aca5d42838e9e
reinaldo <lbmletreiros@gmail.com>
2023-09-05 21:08:31 -0300

    add telas51

---
+++ assets/telas51.js
@@ -0,0 +1,155 @@
+
+function mudarTela51() {
+  switch (tela) {
+    case 1 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/3367859603eeea7791e5ca2d8cef62a5/11e29c050f9db082-67/s1280x1920/c0268d3b6eb040ebf42068d3be8428b1214e96ba.jpg')";
+      break;
+    case 2 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/35057b82d705f8ab4203b0f88eaf6362/c3533e513876c9b8-55/s1280x1920/fcc70575362eb4ddcce41298b5c6d35cd7f4bd1e.jpg')";
+      break;
+    case 3 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/08058ebf840e58e72cb3761f57731ebd/7c96cb7d946a1e9b-12/s1280x1920/4c10d1ae2174394658a1300ba43a3478a639e4e3.jpg')";
+      break;
+    case 4 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/382e6c4d33b5e7e65d138a702eb88839/67301f88c6a96124-17/s1280x1920/78dafb369c64c3973a7f52a1a1aecae0b242ea45.webp')";
+      break;
+    case 5 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/cc52a481db820b9cefe56332de9f38cb/ee1b738e630df159-62/s1280x1920/d43948718adf1d10d97fc7f24b90566693809eff.jpg')";
+      break;
+    case 6 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/1e8a67af55c1be6f2b389e13b8317681/abcf7255856da4ab-7f/s640x960/0e98a5cc267cd5f1a8fed3909c75ed4c3aed1171.jpg')";
+      break;
+    case 7 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/69a785b4e354213a5d264b689d82bde4/c04969e2196a350a-05/s1280x1920/e1e0c3ef95fe27353ac3006e9c48bf27f66fe267.pnj')";
+      break;
+    case 8 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/45d0da1df54997a087d3a6d456a3814c/e1faa87f87637749-d1/s1280x1920/899ccdeaadcfb46545da834f2623fdee9050b1d5.pnj')";
+      break;
+    case 9 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/236e85ba9029444491f5042ae7d3940a/07957f7b4ba22189-eb/s500x750/d255326ef7c690ec49d2d545af1e7bc2e19299de.jpg')";
+      break;
+    case 10 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/70cfd74ab3fcaeecfd2d7510a8ce2cdf/a0f32cb556a2f3f4-7c/s1280x1920/41db089783a602b77936624c986db856f1bf8198.jpg')";
+      break;
+    case 11 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/025c61bd3ebc0aa14bf67c75e0ff88ad/6a5ae22d21c2369e-00/s640x960/0d5d36e56ce46176c0c4fe85b765cd4aefb62361.jpg')";
+      break;
+    case 12 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/31027fe2bfee8a1824e7555a9971597c/d748aac14a957420-bf/s1280x1920/9f5832a6b00254526ad73ca5dae2d973c65b70ca.jpg')";
+      break;
+    case 13 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/cd527711c0b14b00ea87b91b74e00f67/862b03318142c00e-4b/s1280x1920/4bcf1b215ad029907bd6b5eec79d8da3cb2e1f26.jpg')";
+      break;
+    case 14 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/3a71433c3f4562f8deaefacb7a762f26/a464850cb9361efc-cd/s540x810/5ed60f4e8064623a833c6e73f63c66be77fb50b0.jpg')";
+      break;
+    case 15 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/1d66d98a83bea958914748b0938da651/687ec0201653febc-c8/s500x750/027f9ec964bc56fc4e42eb70bd0d67a426711108.pnj')";
+      break;
+    case 16 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/60109b87df9128328e5d370b9423ebf9/13f6bafa0c2a705f-4c/s1280x1920/0a504270f4fe2d62a3e2d64910d3a354b9c22df5.jpg')";
+      break;
+    case 17 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/189786e2f38ace3a008563df6ffeab1f/855ddb38ab2c6a80-a0/s1280x1920/3f6deca6ee56841e2900ff2b965b7e4969168fb0.jpg')";
+      break;
+    case 18 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/358585e984a6fc900a809962c84ec653/86858a61884cc953-ff/s640x960/78312dfaa5a4f92489d1ebac7569f3bd4b9cf4cf.pnj')";
+      break;
+    case 19 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/cfa8ed9b1e18dab31bcaec50c4deb836/4347b60879ffcfb0-b4/s2048x3072/17cade188c56057f51ca75ed2f3193fd5d2b177a.jpg')";
+      break;
+    case 20 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/356eb5e778858db1d0d74176776704e2/4347b60879ffcfb0-b7/s1280x1920/1ee1e635f16c1f62e102ff4f92407297391d9596.jpg')";
+      break;
+    case 21 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/16828680f84eeb536d3ff5f28a2491ff/13f6bafa0c2a705f-7f/s500x750/f4ba302e58cfd2e0317ff3db7832f08b087e0fcd.jpg')";
+      break;
+    case 22 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/970b16b60c1f697057c5da46af80c2c1/92de0b22d1177ec8-4b/s640x960/64c064bd942d951727c79e29b5bf93978b81a844.jpg')";
+      break;
+    case 23 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/95e863fe07fafa1b8dc641faf2106f08/de934f09753193ef-75/s1280x1920/cabd3e292d5c813a6643b525f58994de85d5b356.jpg')";
+      break;
+    case 24 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/f5a99b096a893e8e415badaae242cd73/6f2da2a7418bb908-92/s1280x1920/ea5ac77c222fa455cbdb1201ed15274298b1f124.jpg')";
+      break;
+    case 25 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/048a3859bae3a68b1499ed5819e77873/10dc135f152e6bb1-4c/s1280x1920/7af198e31155c4d992ccfdb7ea75672a2c63dbd3.jpg')";
+      break;
+    case 26 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/6d8537250df1c6149a52b2e060e04130/5723a8ac08aa7770-88/s640x960/894d43d020fbed47484b95d726db52c4004b9761.jpg')";
+      break;
+    case 27 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/6429bad427fc2a522bb1f08fb33b4c95/49bf0551688f9d82-f0/s540x810/45a5886ce52605bc8824bf7c81acaaa9a710de62.jpg')";
+      break;
+    case 28 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/12a1509c7473f9b2d43012ba4cfb784b/54b813d6b85dbb30-77/s2048x3072/8dbb1cb90fbffa8a53b6fd7fd956a63438b1aa51.jpg')";
+      break;
+    case 29 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/3240afd8001c9cf4f770507ef02ac4dd/ec5f609b97db0313-91/s1280x1920/e4612c8fd4b35cea05c34c233601b5d7cf4082cb.jpg')";
+      break;
+    case 30 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/defb7084afdc5c3ceaa4b3fbb8ba79f3/b581a26f8d945f69-87/s540x810/bceef4a50322d42dffb259b97fdd7bc20e140b9e.jpg')";
+      break;
+    case 31 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/62cd8f01dc1a57062610848f05ccbffe/2f3e6e2730e8d45d-68/s1280x1920/9520ff48f5ec1360c03d5de2baf4a3dbda2d88c6.pnj')";
+      break;
+    case 32 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/8a1c41ec36bb5965d5134d0e3a7f06f6/63bfcb35ede7c55d-35/s640x960/eb3c2d6088ccee158f45ae52f4e9043a463687c4.jpg')";
+      break;
+    case 33 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/f229b1809b7932187f7b93b3eb3117e6/1581b49e917f38f6-e4/s400x600/708978bf1e8462878d2c60fe5aaf2f5b2f8d3409.jpg')";
+      break;
+    case 34 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/a89b940a73c0b7604e7129ea448cfbf6/f186844c41a563f8-6b/s640x960/e25a7ef86c554c501a62600fe1ccc751f17c4a94.jpg')";
+      break;
+    case 35 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/79d890bcd8c2c5bdaccf67590a24654b/9cf1b0b1e36edc2e-25/s400x600/72230316314f5fa97b1a3dc1ba737492dd94c166.jpg')";
+      break;
+    case 36 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/98b421f9ef643312470688e4c4567b3c/05ffd9d98172e595-bf/s1280x1920/a19c763624c4feea243da7bf837f89ee379a69ca.jpg')";
+      break;
+    case 37 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/4485ae49775844396f51343c81266ae2/d19c1c6ca5fcac8e-b6/s1280x1920/ee72af8b51da8b847cb4f8ecda45a64a67ac0409.jpg')";
+      break;
+    case 38 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/f25de8f7346fb63f22adf912b901a35b/80fb7d89aad3a2a5-64/s2048x3072/94ff3bc56e706b8995c3bc80371edddf2d897e13.jpg')";
+      break;
+    case 39 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/f944244325b6b5724044533e3dfcef18/e337af485d9ce7bc-7b/s1280x1920/e70449ca6d3433dc7d5e97821b8530519b85d810.jpg')";
+      break;
+    case 40 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/0e567cfc8cd7e0ee1961b762b2113ed7/7736ffe8eb87a954-1c/s1280x1920/2ca9c1596d0175965c51c1691745bb72850423ca.pnj')";
+      break;
+    case 41 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/598b3562f5f7bb7f6080cf01ecccf483/80e52f389d832c34-56/s500x750/aa9bedcd583de6241fcea33ce23148580eda8438.jpg')";
+      break;
+    case 42 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/9c9f603493ff6ed79db429787cc501d1/68902439e795c873-51/s1280x1920/1cc4b01356315e6138de7a8c728d6bad4b5b0554.jpg')";
+      break;
+    case 43 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/5836f9429a65b98f14a4413f52a9779c/dbe9123c9ac766b2-42/s1280x1920/5e211ee46543f24ce5558b8e3bb33214fa11b5f0.jpg')";
+      break;
+    case 44 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/3f56301e1d7c8bd9b4ac1f68242a45a9/938683ff8dc3b37a-ac/s1280x1920/3d311494f6414050c7b540c1cbd25b8b05575e17.pnj')";
+      break;
+    case 45 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/088369f6390ab60e8b99f4a7b2846188/7200fa6a5302899f-9a/s640x960/3c83a5647e1a47c5776c10eed9052eb9fb1229fd.jpg')";
+      break;
+    case 46 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/118e334fade84a31dbbc433e8147fca9/b0e9bd2d34aef601-6b/s640x960/0fcc89236695ef8c1654bbff23f3c76ef9881666.jpg')";
+      break;
+    case 47 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/a5f78f244f89ecf781f681ff7801a1e7/334c68d89bb61a7d-bc/s1280x1920/d8baceb37f79425baec986e6036d350e44182bc3.jpg')";
+      break;
+    case 48 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/ea9c4095b753830054a1b72a905d0235/f980e9a67e3295d4-72/s2048x3072/74d86bfe5070b803b446b32026477dbaf14d6398.pnj')";
+      break;
+    case 49 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/2f52a086e8092e38975408b11af4a118/e8736e04c0f0cbfc-e9/s1280x1920/efb15f3f13f9b09099c3ce1d8ab1877f9f7140d5.jpg')";
+      break;
+    case 50 + nivel:
+      document.body.style.backgroundImage = "url('https://64.media.tumblr.com/0ba49ef40336094a96dcec2b96878f78/aba23d0c90907fa2-a0/s640x960/9028335e522df75753fdcca8420d487d1a646e21.jpg')";
+      break;
+  }
+}
